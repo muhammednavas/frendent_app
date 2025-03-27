0b73e0f3d4a36631b9c2bc62e8164aaf3c8b441f
Muhammednavas <69744754+muhammednavas@users.noreply.github.com>
2025-03-27 10:52:28 +0000

    Updated Login and Signup Apis

--- frendent/app/(auth)/index.jsx
+++ frendent/app/(auth)/index.jsx
@@ -8,21 +8,27 @@ import {
   KeyboardAvoidingView,
   ScrollView,
   Platform,
+  Alert
 } from "react-native";
 import { Link } from "expo-router";
 import styles from "../../assets/styles/login.styles";
 import { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import COLORS from "../../constants/colors";
+import { useAuthStore } from "../../store/authStore";
+
+
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const { isLoading, login } = useAuthStore();
+  
+  const handleLogin = async () => {
+   const result = await login(email, password );
 
-  const handleLogin = () => {
-   
+    if(!result.success) Alert.alert("Error", result.error);
   };
 
   return (
--- frendent/app/index.jsx
+++ frendent/app/index.jsx
@@ -1,21 +1,25 @@
 import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { useAuthStore } from "../store/authStore";
 import { useEffect } from "react";
 
-
 export default function Index() {
- const {user, token, checkAuth} = useAuthStore();
+  const { user, token, checkAuth, logout } = useAuthStore();
 
   useEffect(() => {
     checkAuth();
-  }, [])
-  
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hello {user?.username}</Text>
 
+      <Text style={styles.title}>token{token}</Text>
       <Text>
+        <TouchableOpacity onPress={logout}>
+          <Text>Logout</Text>
+        </TouchableOpacity>
+
         <Link href="/(auth)/signup">Signup Page</Link>
       </Text>
       <Text>
